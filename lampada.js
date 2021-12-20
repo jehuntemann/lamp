c201bb78779b55693df35163533572174aff49de
Jéssica Huntemann <jehhuntemann@gmail.com>
2021-12-20 17:36:23 -0300

    um botão

--- lampada.js
+++ lampada.js
@@ -5,22 +5,30 @@ const lamp = document.getElementById ( 'lamp')
 function islampBroken () {
     return lamp.src.indexOf ('quebrada') > -1 
 }
+function isOff () {
+    return lamp.src.indexOf ( 'desligada' ) >-1
+}
 function lampOn () {
     if ( !islampBroken () ) {
-    lamp.src = './img/ligada.jpg';
+        if (isOff())  {
+            lamp.src = './img/ligada.jpg';
+            turnOn.textContent = 'Desligar'
+        }  else {
+            lamp.src = './img/desligada.jpg';
+            turnOn.textContent = 'Ligar'
+        }
     }
 }
-function lampOff (){
+function lampOff () {
     if ( !islampBroken () ) {
-    lamp.src = './img/desligada.jpg';
+        lamp.src = './img/desligada.jpg';
     }
 }
 function lampBroken () {
     lamp.src = './img/quebrada.jpg'; 
 }
 
-turnOn.addEventListener ( 'click', lampOn )
-turnOff.addEventListener ( 'click', lampOff);
+turnOn.addEventListener ( 'click', lampOn)
 lamp.addEventListener ( 'mouseover', lampOn);
 lamp.addEventListener ( 'mouseleave', lampOff); 
 lamp.addEventListener ( 'dblclick', lampBroken);
